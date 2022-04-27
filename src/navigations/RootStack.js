ceb86a5f10a7c51b51e446bc94b4a516408e97cf
unknown <arnonbass@hotmail.com>
2022-04-27 10:25:05 +0700

    add document

--- src/navigations/RootStack.js
+++ src/navigations/RootStack.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import Home from './../screens/Home';
+import AnimateAPI from '../screens/AnimateAPI';
 
 const Stack = createStackNavigator()
 
 const RootStack = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName='AnimateAPI'>
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="AnimateAPI" component={AnimateAPI} />
     </Stack.Navigator>
   )
 }
